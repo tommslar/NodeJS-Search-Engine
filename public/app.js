dec056d26fb46c6909b1f61d3cbdcac9fb5b4881
Tomas Larrauri <tomas.larrauri@melon.tech>
2018-08-10 14:29:10 -0300

    cargando propiedades en inicio

--- public/app.js
+++ public/app.js
@@ -60,3 +60,54 @@ cargarCiudades();
 setTimeout(()=>{
     $('select').material_select();
   },1000);
+
+
+//cargar todas las propiedades
+function cargarPropiedades(){
+  $.ajax({
+    url:'http://localhost:3000/todos',
+    type:'GET',
+    data:{},
+    success: function (data){
+      var $propiedades = $("#propiedades"); 
+      $.each(data, (i,propiedad)=>{
+        $('#contenedor').append(
+          `<div class="card horizontal">
+          <div class="card-image">
+            <img src="img/home.jpg">
+          </div>
+          <div class="card-stacked">
+            <div class="card-content">
+              <div>
+                <b>Direccion: </b><p>`+propiedad.Direccion+`</p>
+              </div>
+              <div>
+                <b>Ciudad: </b><p>`+propiedad.Ciudad+`</p>
+              </div>
+              <div>
+                <b>Telefono: </b><p>`+propiedad.Telefono+`</p>
+              </div>
+              <div>
+                <b>Código postal: </b><p>`+propiedad.Codigo_Postal+`</p>
+              </div>
+              <div>
+                <b>Precio: </b><p>`+propiedad.Precio+`</p>
+              </div>
+              <div>
+                <b>Tipo: </b><p>`+propiedad.Tipo+`</p>
+              </div>
+            </div>
+            <div class="card-action right-align">
+              <a href="#">Ver más</a>
+            </div>
+          </div>
+        </div>`
+          )
+      })  
+    }
+  });
+}
+
+
+//ejecutar carga de propiedades
+cargarPropiedades()
